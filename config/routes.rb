52b16563706058290591df2af0097bfb0f95a235
sergio santana <sergiosantana.m@gmail.com>
2023-08-16 11:27:42 -0400

    task manager challeng

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get '/tasks/new', to: 'tasks#new', as: :new_task
   post '/tasks', to: 'tasks#create'
   get '/tasks/:id', to: 'tasks#show', as: :task
+  get '/task/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/task/:id', to: 'task#ubdate'
 end
